fbcf5c50da04de2fad6a3b0792f593a433bfc346
yaswanthkumarbollineni <bollineniyaswanthkumar3@gmail.com>
2021-01-18 13:32:31 +0530

    Added method(Print UserName) in Helloworld class

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 class Helloworld{
     public static void main(String[] args) {
         System.out.println("Hello world");
+        printUserName("Yaswanth");
+    }
+    public static void printUserName(String name) {
+        System.out.println("Hello "+name);
     }
 }
