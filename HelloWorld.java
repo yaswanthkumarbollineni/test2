976bec9a026b82edb4894d405962e5b6e5b6239f
yaswanthkumarbollineni <bollineniyaswanthkumar3@gmail.com>
2021-01-18 13:42:56 +0530

    Removed Helloworld file

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-class Helloworld{
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-        printUserName("Yaswanth");
-    }
-    public static void printUserName(String name) {
-        System.out.println("Hello "+name);
-    }
-}
